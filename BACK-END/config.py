a2c8ee77b91b4a02c0a6e6a5aa47d888af025162
Kelly985 <86683835+Kelly985@users.noreply.github.com>
2024-01-29 08:43:46 -0500

    Update database url

--- BACK-END/config.py
+++ BACK-END/config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONs=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI ="postgresql://ireporter_db_user:feX1agtrdJN9Wtjaa6CRCSQcXCxpZsZT@dpg-cjms21fjbvhs73ecjqq0-a.oregon-postgres.render.com/ireporter_db"
+    SQLALCHEMY_DATABASE_URI ="postgresql://ireporter_postgres_db_user:oGMFGoTk5IsUKq8AalmhAttx5mYdz9bb@dpg-cmr2fiq1hbls73fmqud0-a.oregon-postgres.render.com/ireporter_postgres_db"
     # +os.path.join(BASE_DIR, 'report.db')
     DEBUG=True
     SQLALCHEMY_ECHO = True
